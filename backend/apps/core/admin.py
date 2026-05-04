5714ca71abcd516ea222c983c5dd910e514094a2
adrian <adrianvinicius.cs@gmail.com>
2026-05-04 19:27:33 -0300

    adicionar UserAdmin customizado e migração para novo modelo de User

--- backend/apps/core/admin.py
+++ backend/apps/core/admin.py
@@ -1,6 +1,61 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
-from .models import Comunidade, Estado, Municipio, Territorio
+from .models import Comunidade, Estado, Municipio, Territorio, User
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    list_display = (
+        "email",
+        "nome",
+        "perfil",
+        "ativo",
+        "is_staff",
+        "is_superuser",
+        "ultimo_login",
+        "date_joined",
+    )
+    list_filter = ("perfil", "ativo", "is_staff", "is_superuser")
+    ordering = ("-date_joined",)
+    search_fields = ("email", "nome")
+    readonly_fields = ("ultimo_login", "date_joined")
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Informações pessoais", {"fields": ("nome", "perfil")}),
+        (
+            "Permissões",
+            {
+                "fields": (
+                    "ativo",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Datas importantes", {"fields": ("ultimo_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "nome",
+                    "perfil",
+                    "password1",
+                    "password2",
+                    "ativo",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
+    filter_horizontal = ("groups", "user_permissions")
 
 
 @admin.register(Estado)
