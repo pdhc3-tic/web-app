530c0bcc393285f5b114b0daba1c5a6d7a44c0f4
Aecio Silva <aeciocruzfcs@gmail.com>
2026-05-12 17:40:32 -0300

    Criado rotas para solicitação e confirmação de reset de senha.

--- backend/setup/urls.py
+++ backend/setup/urls.py
@@ -20,7 +20,7 @@ from django.urls import include, path
 from rest_framework_simplejwt import views as jwt_views
 from rest_framework_simplejwt.views import TokenBlacklistView
 
-from .views import LoginView, RefreshView, LogoutView, logout_all, me
+from .views import LoginView, RefreshView, LogoutView, logout_all, me, password_reset_request, password_reset_confirm
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -40,4 +40,6 @@ urlpatterns = [
          LogoutView.as_view(), 
          name="token_blacklist"),
     path("api/v1/auth/logout-all/", logout_all, name="logout_all"),
+    path("api/v1/auth/password-reset/request/", password_reset_request, name="password_reset_request"),
+    path("api/v1/auth/password-reset/confirm/", password_reset_confirm, name="password_reset_confirm"),
 ]
