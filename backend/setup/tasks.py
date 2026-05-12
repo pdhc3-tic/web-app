a35d8c5bfb199068e679b1ec43d20c9851145335
Aecio Silva <aeciocruzfcs@gmail.com>
2026-05-12 17:45:45 -0300

    Criação da task de envio de email.

--- backend/setup/tasks.py
+++ backend/setup/tasks.py
@@ -1,7 +1,19 @@
 from celery import shared_task
 from django.core.management import call_command
+from django.core.mail import send_mail, EmailMessage
+from django.conf import settings
 
 
 @shared_task
 def cleanup_expired_tokens():
     call_command("flushexpiredtokens")
+
+@shared_task
+def send_email_notification(subject, message, recipient_list):
+    send_mail(
+        subject=subject,
+        message=message,
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        recipient_list=recipient_list,
+        fail_silently=False,
+    )
