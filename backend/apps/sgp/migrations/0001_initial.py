fed44bc32aef0697d4246a3e43ed4db9ee76bb94
Adrian <adrianvinicius.cs@gmail.com>
2026-07-08 11:34:35 -0300

    fix: corrige dependencia da migration sgp.0001 para core.0015_alter_auditlog_and_notification

--- backend/apps/sgp/migrations/0001_initial.py
+++ backend/apps/sgp/migrations/0001_initial.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('core', '0015_alter_notification_enviado_em_and_more'),
+        ('core', '0015_alter_auditlog_and_notification'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
