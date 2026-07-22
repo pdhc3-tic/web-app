fcce19bdcc1bd0566f223a52b48b3400ac068110
Adrian <adrianvinicius.cs@gmail.com>
2026-07-22 15:46:33 -0300

    Consolida rotas de atividades no apps/sgp/urls.py eliminando activity_urls.py

--- backend/apps/sgp/urls.py
+++ backend/apps/sgp/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import (
+    ActivityViewSet,
     ComunidadeViewSet,
     CulturaListView,
     EspecieAnimalListView,
@@ -19,6 +20,9 @@ router.register("projetos", ProjetoViewSet, basename="projeto")
 router.register('comunidades', ComunidadeViewSet, basename='comunidade')
 router.register("metas", WorkPlanMetaViewSet, basename="workplanmeta")
 router.register("acoes", WorkPlanAcaoViewSet, basename="workplanacao")
+
+sgp_router = DefaultRouter()
+sgp_router.register("atividades", ActivityViewSet, basename="atividade")
 
 comunidade_list = ComunidadeViewSet.as_view({
     'get': 'list',
@@ -48,6 +52,7 @@ production_detail = ProductionViewSet.as_view({
 })
 
 urlpatterns = router.urls + [
+    path("sgp/", include(sgp_router.urls)),
     path(
         "catalogos/culturas/",
         CulturaListView.as_view(),
